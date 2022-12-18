4ff7061d9be33b48c9c752e9b5d349fda2a11e79
kjmfum <kjmfum@yahoo.co.uk>
2022-12-18 05:29:26 +0000

    add comments to functions

--- image-filter-starter-code/src/server.ts
+++ image-filter-starter-code/src/server.ts
@@ -27,19 +27,21 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
   /**************************************************************************** */
 
-    // filteredimage?image_url=https://upload.wikimedia.org/wikipedia/commons/9/94/Morena_pimienta_%28Gymnothorax_griseus%29%2C_parque_nacional_Ras_Muhammad%2C_Egipto%2C_2022-03-27%2C_DD_185.jpg
-  // http://project-dev.us-east-1.elasticbeanstalk.com/filteredimage?image_url=https://assetsnffrgf-a.akamaihd.net/assets/m/502000286/univ/art/502000286_univ_lsr_lg.jpg
-  // http://localhost:8082/filteredimage?image_url=
-  // http://project-dev.us-east-1.elasticbeanstalk.com/filteredimage?image_url=https://upload.wikimedia.org/wikipedia/commons/b/bd/Golden_tabby_and_white_kitten_n01.jpg
-  // http://project-dev.us-east-1.elasticbeanstalk.com/filteredimage?image_url=https://inaturalist-open-data.s3.amazonaws.com/photos/72249626/medium.jpeg
 
+  // isImage
+// helper function to validate if a url is an image url
+// returns true if url is image or false if url is not image
+// INPUTS
+//    url: string - a publicly accessible url
+// RETURNS
+//    a boolean indicating true if url is valid image url or false if not
   function isImage(url : string) {
     return /^https?:\/\/.+\.(jpg|jpeg|png|webp|avif|gif|svg)$/.test(url);
   }
 
   app.get("/filteredimage/", async (req, res)=>{
    let { image_url } = req.query;
-  // check if image url is correctly formatted
+  // validate image_url
       if(isImage(image_url)) {
         try {
           const filteredPath = await filterImageFromURL(image_url);
